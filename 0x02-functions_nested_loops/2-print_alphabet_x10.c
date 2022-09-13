ecab9780304fc4d20519e963d74b1acae25ca204
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-13 16:15:40 -0700

    Printing the english alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print alphabet 10 times - Print the english alphabet in 10 rows
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int r = 1;
+
+	while (r < 11)
+	{
+		char c = 'a';
+
+		while (c < 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		r++;
+	}
+}
