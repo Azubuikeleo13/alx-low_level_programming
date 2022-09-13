28140d2d7cc964e6238f36866bcc1c25ecb07adf
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-13 15:55:24 -0700

    Printing the english alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the english alphabet starting from a-z
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
