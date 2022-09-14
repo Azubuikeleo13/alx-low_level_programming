04648e3471b0280841ab3ad52ade17dfe28e660c
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-13 17:03:27 -0700

    Checking Negative and positive numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,10 +4,9 @@
  * print_sign - print the sign of a number
  * @n: type int integer, can be negative or positive
  * Description: print +, 0, or - depending on number, along with return
- *
  * Return: 1 and prints + if n is greater than zero
- * 	   0 and prints 0 if n is zero
- *	  -1 and prints - if n is less than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
