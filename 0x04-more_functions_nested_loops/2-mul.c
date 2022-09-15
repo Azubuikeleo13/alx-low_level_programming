11823f1fccfca277efcca4cdaa23e213c288dac9
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-15 10:37:13 -0700

    Getting the product of two number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplying two numbers
+ * @a: checking the first number
+ * @b: checking the second number
+ * Return: res
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return(res);
+}
