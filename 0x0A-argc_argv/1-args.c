21f10ce4af52f9f83dfc7b527f7bfc37fb1f0672
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-30 15:35:00 -0700

    Getting the total number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
+#include "main.h"
+
 /**
- * main - Entry point
- * @argc: number of argument passed
- * @agrv: argument passed into it
- * Return: 0
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d/n", argc);
+	if (argc > 0)
+
+		printf("%d\n", argc - 1);
 
 	return (0);
 }
