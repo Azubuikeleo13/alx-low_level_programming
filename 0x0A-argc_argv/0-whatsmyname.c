7cd8590b70fb707e52f032e377d73cb5bb18563b
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-30 15:23:22 -0700

    Correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - function
  * @argc: offset count
  * @argv: offset value
- * Return: always 0
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv(argc -1);
+	printf("%s\n", argv[argc - 1]);
+
 	return (0);
 }
