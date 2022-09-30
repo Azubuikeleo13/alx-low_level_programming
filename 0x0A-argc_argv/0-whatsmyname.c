46717cc94651784c981317425634c66eafeaf146
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-30 14:27:11 -0700

    First commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Entry point
- *
+ * @argc: offset count
+ * @argv: offset value
  * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", *(argv + (argc -1));
+	printf ("%s\n", *(argv(argc -1));
 	return (0);
 }
