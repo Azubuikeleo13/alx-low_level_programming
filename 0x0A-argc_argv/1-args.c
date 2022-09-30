ea326b488708b8e99c3bf24c7971d27253d311b0
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-09-30 15:30:29 -0700

    Getting the total number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of argument passed
+ * @agrv: argument passed into it
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d/n", argc);
+
+	return (0);
+}
