413d36f9073cc9f329f99b5a6f69c1753baaecb9
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-07 01:30:34 -0700

    Function to print range of arrays

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * array_range - Function to print range of arrays
+ * @min: Minimum number
+ * @max: Maximum number
+ *
+ * Return: Pointer to the new created array
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *s;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	s = malloc(sizeof(int) * (max - (min)));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	for (; i <= (max - (min)); i++, min++)
+	{
+		*(s + i) = min;
+	}
+	return (s);
+}
