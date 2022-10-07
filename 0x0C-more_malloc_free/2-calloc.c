11a239443e5b763645548050889a0c48b7485207
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-07 00:52:06 -0700

    Function to create memory space for n member

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,22 +10,24 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i;
-	char *s;
+	unsigned int i;
+	char *arr;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	s = malloc(nmemb * size);
-	if (s == NULL)
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	i = 0;
-	for (; i <= nmemb; i++)
+
+	for (i = 0; i < (nmemb * size); i++)
 	{
-		s[i] = 0;
+		arr[i] = 0;
 	}
-	return (s);
+
+	return (arr);
 }
