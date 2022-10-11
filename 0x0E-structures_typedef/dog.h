ab8f40e7d2013493431150fa155d5deb6320d06e
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-11 00:40:58 -0700

    My Readme and my first task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defines a dog
+ * @name: the dog's name
+ * @owner: the dog's owner
+ * @age: the dog's age
+ */
+
 struct dog {
 	char *name;
 	float age;
