0d504e4240b7606fa6692947b50b9bc44ce67ad5
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-11 02:25:37 -0700

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,17 +11,18 @@
 struct dog
 {
 	char *name;
-	float age;
 	char *owner;
+	float age;
 };
 
-/**
- * Function Prototypes
- */
+typedef struct dog dog_t;
+
+/*function protypes*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strdup(char *str);
 
-#endif /* DOG_H */
+#endif /*DOG_H*/
