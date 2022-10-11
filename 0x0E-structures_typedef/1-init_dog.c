273cab86f5f824b3848466acdbed711c7b22fe98
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-11 01:40:07 -0700

    Initializing a Structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,12 +13,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d;
-
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
-		return (NULL);
+		d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		exit(EXIT_FAILURE);
+
 	d->name = name;
-	d->age = age;
 	d->owner = owner;
+	d->age = age;
 }
