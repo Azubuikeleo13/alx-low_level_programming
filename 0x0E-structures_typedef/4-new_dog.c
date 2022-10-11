b3c4497ad10abf336b0f354aa34c638a828c8707
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-11 03:44:33 -0700

    Creating a new struct with copying data from the other Struc

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,83 @@
+#include "dog.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: the new dog's name
+ * @age: the new dog's age
+ * @owner: the new dog's owner
+ *
+ * Return: a pointer to the new dog's stucture
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pupp;
+
+	pupp = malloc(sizeof(dog_t));
+
+	if (pupp == NULL)
+		return (NULL);
+
+	pupp->name = _strdup(name);
+
+	if (!(pupp->name))
+	{
+		free(pupp);
+
+		return (NULL);
+	}
+	pupp->owner = _strdup(owner);
+
+	if (!(pupp->owner))
+	{
+		free(pupp->name);
+
+		free(pupp);
+
+		return (NULL);
+	}
+	pupp->age = age;
+
+	return (pupp);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: the string to copy
+ *
+ * Return: a pointer to the new string on success,
+ * otherwise NULL
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int len = 0, i = 0;
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+	{
+		len++;
+	}
+	new_str = (char *) malloc(sizeof(char) * (len + 1));
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i < len; i++)
+		{
+			new_str[i] = str[i];
+		}
+	}
+	return (new_str);
+}
