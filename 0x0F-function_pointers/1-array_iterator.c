2c5bc193cbf1fcb6ba0e0601fa960f4257a1e00b
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-12 09:24:44 -0700

    Function to act on a single element

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
-
-	if (size < 1 || array == NULL || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
-	for (i = 0; i <= size; i++)
-		action(array[i]);
+	while (size-- > 0)
+	{
+		action(*array);
+
+		array++;
+	}
 }
