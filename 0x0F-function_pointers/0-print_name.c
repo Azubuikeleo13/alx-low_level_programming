b3b81b362a4614374b9511ee840043257f4d944e
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-12 08:35:00 -0700

    Function pointer to print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Function to print a name
+ * print_name - Prints a name.
+ * @name: The name to be printed.
  *
- * @name: String to print
- * @f: Pointer to a function
- *
- * Return: void
+ * @f: A pointer to a function that prints a name.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-
 		return;
-
 	f(name);
 }
