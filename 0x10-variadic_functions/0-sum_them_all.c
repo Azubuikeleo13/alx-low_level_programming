9be73e65db5a19b57e48eb2e6c458a575df19da7
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-13 15:40:36 -0700

    Getting the sum of all passed Arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,21 +14,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
+	va_list nums;
 
-	unsigned int i, sum;
+	unsigned int index, sum = 0;
 
-	if (n == 0)
-		return (0);
+	va_start(nums, n);
 
-	va_start(ap, n);
-	sum = 0;
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(ap, int);
-	}
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
 
-	va_end ap;
+	va_end(nums);
 
 	return (sum);
 }
