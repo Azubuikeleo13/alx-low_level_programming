39080e1999448d83f8da384a98db4a9b6375cbb5
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-13 13:34:18 -0700

    updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,19 +1,21 @@
 #include "3-calc.h"
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - entry point
- * @argc: the number of arguments passed
- * @argv: a pointer to an array of arguments
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success
+ * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*operator)(int, int);
+	int num1, num2;
+
+	char *op;
 
 	if (argc != 4)
 	{
@@ -21,16 +23,26 @@ int main(int argc, char *argv[])
 
 		exit(98);
 	}
-	operator = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
 
-	if (operator == NULL)
+	op = argv[2];
+
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) ||
+
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 
-		exit(99);
+		exit(100);
 	}
-	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
-
 }
