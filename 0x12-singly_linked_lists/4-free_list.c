9901ed41b4cc065059b3d93928ecbd578547f3e7
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-20 16:17:03 -0700

    Freeing a linked list memory space

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,13 +10,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *tmp = head;
+	list_t *temp;
 
-	while (tmp)
+	while (head)
 	{
-		free(tmp);
-		tmp = tmp->next;
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
-
-	free(tmp);
 }
