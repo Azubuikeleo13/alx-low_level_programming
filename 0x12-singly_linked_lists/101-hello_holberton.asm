c2c2d5d8bd112ac04e16b92d7bb2b480b5b098ab
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-21 15:15:27 -0700

    Writing Assembly code in C

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,4 +1,3 @@
-
 global _start
 
 section .text:
@@ -7,10 +6,13 @@ _start
 	mov eax, 0x4
 	mov ebx, 1
 	mov ecx, message
-	mov edx, 16
+	mov edx, message_len
+	int 0x80
 
 	mov eax, 0x1
 	mov ebx, 0
+	int 0x80
 
 section .data:
-        message: db "Hello, Holberton", 0xA
+	message: db "Hello, Holberton", 0xA
+	message_len equ $-message
