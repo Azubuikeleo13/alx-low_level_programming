e822699bcb3a67cb2bbfb498df1dc89469e8a083
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-10-31 06:35:19 -0700

    Reading and writing a file to Standard output, Updated Header File

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfiile - Function to read text file
+ *
+ * @filename: pointer to the name of the file
+ * @letters: Number of letters to be write and write to STDOUT
+ *
+ * Return: Number of letters read and written on successful
+ *         0 if not successful.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+
+	0 = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
