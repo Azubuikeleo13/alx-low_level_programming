aa36a1cbccdf51d785b1103e43866b4544d912cb
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-08 05:30:28 -0800

    Freeing a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function to free up a list
+ *
+ * @head: Pointer to the head of the list
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head->next != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+	free(head);
+	return;
+}
