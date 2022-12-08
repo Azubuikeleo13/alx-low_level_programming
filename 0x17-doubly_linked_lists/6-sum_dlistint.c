31a9aa7eb436b0eb573258d284c34da2deb088b9
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-08 07:07:22 -0800

    The sum of values in list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -14,12 +14,12 @@ int sum_dlistint(dlistint_t *head)
 	int sum = 0;
 
 	if (!(head))
-		return 0;
+		return (0);
 
 	while (head)
 	{
 		sum += head->n;
 		head = head->next;
 	}
-	return sum;
+	return (sum);
 }
