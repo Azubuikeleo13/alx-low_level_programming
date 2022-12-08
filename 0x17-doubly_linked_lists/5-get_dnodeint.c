f0c153fe85e4331fe0434f12e6763bed7f9660dc
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-08 06:51:44 -0800

    Getting a node at an index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,26 +12,12 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t temp;
-	unsigned int count == 0;
-
-	if (head == NULL)
-		return NULL;
-
-	temp = *head;
-
-	while (temp)
+	for (; index != 0; index--)
 	{
-		count++;
-		temp = temp->next;
-	}
-	if (count > index)
-		return NULL;
+		if (head == NULL)
+			return (NULL);
 
-	while (index >= 0)
-	{
 		head = head->next;
-		index--;
 	}
-	return head;
+	return (head);
 }
