0b8e7b4ae1b53f96878bdbddfe174a3eaedbd491
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-09 03:27:11 -0800

    Printing the elements of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pprint_listint - Function to print a list
+ *
+ * @h: Pointer to the head of the list
+ *
+ * Return: the data item of the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while(h)
+	{
+		count++;
+		printf("%d", h->n);
+		h = h->next;
+	}
+	return (count);
+}
