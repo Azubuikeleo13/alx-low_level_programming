c2f79ee1532b2785b05a7ac06a02afebc0dc6d52
‘Azubuikeleo13’ <azubuikeleo13@gmail.com>
2022-12-09 04:24:58 -0800

    Adding a node at the head of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t tmp;
+	listint_t *tmp;
 
 	tmp = malloc(sizeof(listint_t));
 
@@ -22,7 +22,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	tmp->n = n;
 	tmp->next == NULL;
 
-	tmp->next = (*head);
+	tmp->next = *head;
 	*head = tmp;
 
 	return (tmp);
